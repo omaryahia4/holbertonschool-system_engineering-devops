54ad4778bbb1f7223759deaf6f4ce697debbbd60
omaryahia4 <omar.yahia4444@gmail.com>
2021-09-13 18:28:46 +0100

    gather data from API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    id = argv[1]
+    url = ('https://jsonplaceholder.typicode.com/users/')
+    employee = requests.get(url + '{}'.format(
+        id)).json()
+    tasks = requests.get(url + '{}/todos'.format(id)).json()
+    complited = 0
+    for task in tasks:
+        if task['completed']:
+            complited += 1
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            employee['name'], complited, len(tasks)))
+    for task in tasks:
+        if task['completed']:
+            print("\t {}".format(task['title']))
