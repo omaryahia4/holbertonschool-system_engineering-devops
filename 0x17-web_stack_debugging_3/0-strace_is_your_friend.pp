571f7064b89d573fbfc65408aa0214b9e9ee411b
omaryahia4 <omar.yahia4444@gmail.com>
2021-09-29 16:09:49 +0100

    apache error fixed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-# Puppet code to fix why Apache is returning 500 error .
+# Puppet code to fix Apache 500 error.
 exec { 'fix-phpp':
 path     => 'usr/bin/:/bin/',
 command  => "sed -i -e 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+provider => 'shell',
 }
