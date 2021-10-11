fd8add56bb90456b9411ad0553f9c3e80ce3b8a5
omaryahia4 <omar.yahia4444@gmail.com>
2021-10-11 12:36:45 +0100

    fix code

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-#  create a file in /tmp:
+#create a file in /tmp:
 file { '/tmp/holberton':
   mode    =>  '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet'
-
 }
